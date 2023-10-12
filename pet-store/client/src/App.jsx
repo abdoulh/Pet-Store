0ce56560b19cdff6cd4d6b814c3e4d7638f074fd
Abderrahmen <abderrahmenlouhichi3@gmail.com>
2023-10-12 18:23:25 +0100

    add protection routes

--- pet-store/client/src/App.jsx
+++ pet-store/client/src/App.jsx
@@ -1,90 +1,112 @@
 import React, { useState, useEffect } from "react";
-import './App.css'
+import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import LandingPage from "./Components/LandingPage.jsx"
-import HomePage from './Components/HomePage.jsx';
-import Cart from "./Components/Cart.jsx"
-import AdminAddProduct from './Components/AdminAddProduct.jsx';
-import AdminProductList from './Components/AdminProductList.jsx';
-import AdminUsersList from './Components/AdminUsersList.jsx';
-import axios from 'axios'
+import LandingPage from "./Components/LandingPage.jsx";
+import HomePage from "./Components/HomePage.jsx";
+import Cart from "./Components/Cart.jsx";
+import AdminAddProduct from "./Components/AdminAddProduct.jsx";
+import AdminProductList from "./Components/AdminProductList.jsx";
+import AdminUsersList from "./Components/AdminUsersList.jsx";
+import axios from "axios";
+import ProtectedRouteAdmin from "./providers/admin.provider.jsx";
+import ProtectedRouteUser from "./providers/user.provider.jsx";
 
 const App = () => {
-  const [products, setProducts] = useState([])
-  const [users, setUsers] = useState([])
-  const [currentUserID, setCurrentUserID] = useState()
-  const [cart, setCart] = useState([])
-
-
+  const [products, setProducts] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [currentUserID, setCurrentUserID] = useState();
+  const [currentUserRole, setCurrentUserRole] = useState(localStorage.getItem("role"));
+  const [cart, setCart] = useState([]);
+  console.log(currentUserRole);
   const fetchAllCartItems = async () => {
     try {
-      const { data } = await axios.get('http://localhost:3000/api/carts/' + currentUserID)
-      setCart(data)
+      const { data } = await axios.get(
+        "http://localhost:3000/api/carts/" + currentUserID
+      );
+      setCart(data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-  }
-
+  };
 
   const fetchProducts = async () => {
     try {
-      const { data } = await axios.get("http://localhost:3000/api/product")
-      setProducts(data)
-      getUserId()
-      fetchAllCartItems()
-      console.log(data)
+      const { data } = await axios.get("http://localhost:3000/api/product");
+      setProducts(data);
+      getUserId();
+      fetchAllCartItems();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
     }
-    catch (error) {
-      console.log(error)
-    }
-
-  }
+  };
 
   useEffect(() => {
-    fetchProducts()
-
-  }, [])
-
+    fetchProducts();
+  }, []);
 
   const getUserId = async () => {
-
     try {
-      const { data } = await axios.get("http://localhost:3000/api/users/payload/" + localStorage.getItem('token'))
-      console.log(data.payload.userId)
+      const { data } = await axios.get(
+        "http://localhost:3000/api/users/payload/" +
+        localStorage.getItem("token")
+      );
+      console.log(data.payload.userId);
       setCurrentUserID(data.payload.userId)
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-
-  }
+  };
+  const token = localStorage.getItem("token")
 
   const addToCart = async (userID, productID) => {
-
     try {
-      const { data } = axios.post("http://localhost:3000/api/carts/" + userID + "/" + productID)
+      const { data } = axios.post(
+        "http://localhost:3000/api/carts/" + userID + "/" + productID
+      );
     } catch (error) {
-      console.log(error)
-
+      console.log(error);
     }
-
-  }
+  };
 
   return (
+
+
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage setCurrentUser={setCurrentUserID} />} />
-        <Route path="/Cart" element={<Cart cart={cart} />} />
-        <Route path="/HomePage" element={<HomePage items={products} addToCart={addToCart} currentUserID={currentUserID} />} />
-        <Route path="/AdminProductList" element={<AdminProductList />} />
-        <Route path="/AdminUsersList" element={<AdminUsersList />} />
-        <Route path="/AdminAddProduct" element={<AdminAddProduct />} />
+        <Route
+          path="/"
+          element={<LandingPage setCurrentUser={setCurrentUserID} />}
+        />
+
+        <Route path="/HomePage" element={<ProtectedRouteUser redirectPath="/HomePage"  isAllowed={ token && currentUserRole.includes("customer")} />}>
+         
+          <HomePage items={products}   addToCart={addToCart}  currentUserID={currentUserID}  />
+                   
+        </Route>
+
+        
+        <Route path ="AdminProductList" element={<ProtectedRouteAdmin redirectPath="/HomePage"  isAllowed={ token && currentUserRole.includes("admin")} >
+          
+        <AdminProductList />
+            </ProtectedRouteAdmin>
+          }>
+      
+        </Route>
+        <Route path="AdminUsersList" element={<ProtectedRouteAdmin redirectPath="/HomePage"  isAllowed={ token && currentUserRole.includes("admin")} >
+          <AdminUsersList />
+            </ProtectedRouteAdmin>
+          }>
+    </Route>
+    <Route path="AdminAddProduct" element={<ProtectedRouteAdmin redirectPath="/HomePage"  isAllowed={ token && currentUserRole.includes("admin")} >
+          <AdminAddProduct />
+            </ProtectedRouteAdmin>
+          }>
+    </Route>
+
       </Routes>
     </Router>
   );
-
-
-
-}
-
+};
 
 export default App;
