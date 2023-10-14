7f0cf20f8a7269053079b3b8c2d80251df065801
Abderrahmen <abderrahmenlouhichi3@gmail.com>
2023-10-14 11:28:08 +0100

    update middleware

--- pet-store/backEnd/middlewares/checkToken.middleware.js
+++ pet-store/backEnd/middlewares/checkToken.middleware.js
@@ -12,16 +12,7 @@ exports.checkTokenMiddleware = async (req, res, next) => {
     var decoded = await jwt.verify(token, process.env.jwt_Secret);
     req.userId = decoded.id;
     req.role = decoded.role;
-
-
-    const allowedRoles = ["customer"]; 
-
-    if (allowedRoles.includes(req.role)) {
-      console.log('qsqssfsqfsqfqsfq');
-      return next(); 
-    } else {
-      return res.status(403).json({ message: 'Access denied' });
-    }
+    
   } catch (error) {
     console.error(error);
     return res.status(401).json(error);
