526f86a5fb050c0b00eefdbf307554fb5cfd7e67
Haithemch98 <chaouchhaithem0@gmail.com>
2023-10-15 20:51:47 +0100

    styling admin pages

--- pet-store/client/src/Components/AdminEditProduct.jsx
+++ pet-store/client/src/Components/AdminEditProduct.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import axios from '../services/axios-interceptor';
-import '../index.css';
+import '../styles/adminEdit.css'
 import { useNavigate } from "react-router-dom";
 
 const AdminEditProduct = ({ selectedProduct }) => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const [editedProduct, setEditedProduct] = useState({
     name: selectedProduct.name,
@@ -25,11 +24,8 @@ const AdminEditProduct = ({ selectedProduct }) => {
 
   const handleImage = (e) => {
     const file = e.target.files[0]
-    setEditedProduct({ ...editedProduct, imageUrl: file })
-
+    setEditedProduct({ ...editedProduct, imageUrl: file });
   }
-
-
 
   const EditProduct = async () => {
     try {
@@ -41,28 +37,23 @@ const AdminEditProduct = ({ selectedProduct }) => {
       formData.append("description", editedProduct.description);
       formData.append("price", editedProduct.price);
 
-
-      const productId = selectedProduct.id
+      const productId = selectedProduct.id;
 
       await axios.put(
         `http://localhost:3000/api/product/${productId}`,
         formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-      navigate(0)
+          headers: {
+            "Content-Type": "multipart/form-data",
+          },
+        });
+      navigate(0);
     } catch (error) {
-
       if (error.response.status === 401) {
-        localStorage.clear()
-        navigate('/Login')
+        localStorage.clear();
+        navigate('/Login');
+      } else if (error.response.status === 403) {
+        navigate('/HomePage');
       }
-      else if (error.response.status === 403) {
-        navigate('/HomePage')
-      }
-
-
     }
   };
 
@@ -71,27 +62,25 @@ const AdminEditProduct = ({ selectedProduct }) => {
     await EditProduct();
   };
 
-
   return (
-    <div id="editProductModal" className="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-      <div className="modal-box">
+    <div id="editProductModal-admin" >
+      <div className="modal-box-admin">
         <h2>Edit Product</h2>
         <form onSubmit={handleSubmit}>
-          <div className="form-group">
-            <label htmlFor="name">Product Name:</label>
+          <div className="form-group-admin">
+            <label htmlFor="name-admin">Product Name:</label>
             <input
               type="text"
-              id="name"
+              id="name-admin"
               name="name"
               placeholder={selectedProduct.name}
               onChange={handleInputChange}
-
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="category">Category:</label>
+          <div className="form-group-admin">
+            <label htmlFor="category-admin">Category:</label>
             <select
-              id="category"
+              id="category-admin"
               name="category"
               placeholder={selectedProduct.category}
               onChange={handleInputChange}
@@ -102,10 +91,10 @@ const AdminEditProduct = ({ selectedProduct }) => {
               <option value="Upholstery">Upholstery</option>
             </select>
           </div>
-          <div className="form-group">
-            <label htmlFor="animal">Animal:</label>
+          <div className="form-group-admin">
+            <label htmlFor="animal-admin">Animal:</label>
             <select
-              id="animal"
+              id="animal-admin"
               name="animal"
               placeholder={selectedProduct.animal}
               onChange={handleInputChange}
@@ -115,38 +104,36 @@ const AdminEditProduct = ({ selectedProduct }) => {
               <option value="cat">Cat</option>
             </select>
           </div>
-          <div className="form-group">
-            <label htmlFor="imageUrl">Image URL:</label>
+          <div className="form-group-admin">
+            <label htmlFor="imageUrl-admin">Image URL:</label>
             <input
-              id="imageUrl"
+              id="imageUrl-admin"
               name="imageUrl"
               type='file'
               placeholder={selectedProduct.imageUrl}
               onChange={handleImage}
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="description">Description:</label>
+          <div className="form-group-admin">
+            <label htmlFor="description-admin">Description:</label>
             <textarea
-              id="description"
+              id="description-admin"
               name="description"
               placeholder={selectedProduct.description}
               onChange={handleInputChange}
-
             />
           </div>
-          <div className="form-group">
-            <label htmlFor="price">Price:</label>
+          <div className="form-group-admin">
+            <label htmlFor="price-admin">Price:</label>
             <input
               type="number"
-              id="price"
+              id="price-admin"
               name="price"
               placeholder={selectedProduct.price}
               onChange={handleInputChange}
-
             />
           </div>
-          <div className="form-group">
+          <div className="form-group-admin">
             <input type="submit" value='Edit Product' />
           </div>
         </form>
